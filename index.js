be00b945a7a64c89335cbb315f516f5c445a7d4d
WillLai <252926752@qq.com>
2019-08-12 14:06:15 +0800

    change body parse to parser

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const bodyParser = require('body-parse');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
