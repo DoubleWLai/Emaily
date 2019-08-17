80aba584e249d99a33f8a3c4a6e39faf188b857b
WillLai <252926752@qq.com>
2019-08-17 16:22:33 +0800

    completed the first version

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
+import { FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user');
---
+++ client/src/components/surveys/SurveyList.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../actions';
+
+class SurveyList extends Component {
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
+
+  renderSurveys() {
+    return this.props.surveys.reverse().map(survey => {
+      return (
+        <div className="card darken-1" key={survey._id}>
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p>
+              {survey.body}
+            </p>
+            <p className="right">
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <a>Yes: {survey.yes}</a>
+            <a>No: {survey.no}</a>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{ textAlign: 'center'}}>
+          <h2>When adding credits please use credit card #</h2>
+          <h3>4242 4242 4242 4242</h3>
+        </div>
+        {this.renderSurveys()}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ surveys }) {
+  return { surveys }
+}
+
+export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
